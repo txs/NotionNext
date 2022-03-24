c93bc7073e7f1e170b607d953a309b8488259eee
Andy TV <andytv@AndydeMacBook-Pro.local>
2022-03-24 13:43:12 +0800

    switch theme

--- blog.config.js
+++ blog.config.js
@@ -36,7 +36,7 @@ const BLOG = {
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: 'andytv08@gmail.com',
   CONTACT_WEIBO: '',
-  CONTACT_TWITTER: '',
+  CONTACT_TWITTER: 'txshon',
   CONTACT_GITHUB: 'https://github.com/txs',
   CONTACT_TELEGRAM: '',
 
