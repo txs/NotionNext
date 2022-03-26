3f55afdc3e3a032fd6b4a178b44b80f6417c76f5
txs <txshon@gmail.com>
2022-03-27 01:47:31 +0800

    giscus prettier

--- components/Giscus.js
+++ components/Giscus.js
@@ -6,22 +6,25 @@ import { Giscus } from '@giscus/react'
  * @returns {JSX.Element}
  * @constructor
  */
+
 const GiscusComponent = ({ isDarkMode }) => {
     const theme = isDarkMode ? 'dark' : 'light'
-    return (<Giscus
-        repo={BLOG.COMMENT_GISCUS_REPO}
-        repoId={BLOG.COMMENT_GISCUS_REPO_ID}
-        category={BLOG.COMMENT_GISCUS_CATEGORY}
-        categoryId={BLOG.COMMENT_GISCUS_CATEGORY_ID}
-        mapping={BLOG.COMMENT_GISCUS_MAPPING}
-        reactionsEnabled={BLOG.COMMENT_GISCUS_REACTIONS_ENABLED}
-        emitMetadata={BLOG.COMMENT_GISCUS_EMIT_METADATA}
-        theme={theme}
-        inputPosition={BLOG.COMMENT_GISCUS_INPUT_POSITION}
-        lang={BLOG.COMMENT_GISCUS_LANG}
-        loading={BLOG.COMMENT_GISCUS_LOADING}
-        crossorigin={BLOG.COMMENT_GISCUS_CROSSORIGIN}
-        />)
+    return (
+        <Giscus
+            repo={BLOG.COMMENT_GISCUS_REPO}
+            repoId={BLOG.COMMENT_GISCUS_REPO_ID}
+            category={BLOG.COMMENT_GISCUS_CATEGORY}
+            categoryId={BLOG.COMMENT_GISCUS_CATEGORY_ID}
+            mapping={BLOG.COMMENT_GISCUS_MAPPING}
+            reactionsEnabled={BLOG.COMMENT_GISCUS_REACTIONS_ENABLED}
+            emitMetadata={BLOG.COMMENT_GISCUS_EMIT_METADATA}
+            theme={theme}
+            inputPosition={BLOG.COMMENT_GISCUS_INPUT_POSITION}
+            lang={BLOG.COMMENT_GISCUS_LANG}
+            loading={BLOG.COMMENT_GISCUS_LOADING}
+            crossorigin={BLOG.COMMENT_GISCUS_CROSSORIGIN}
+        />
+    )
 }
 
 export default GiscusComponent
