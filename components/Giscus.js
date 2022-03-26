2a576f98d6ca975563088443c96560fc25e5e639
txs <txshon@gmail.com>
2022-03-27 01:38:23 +0800

    fix some indent issue

--- components/Giscus.js
+++ components/Giscus.js
@@ -1,14 +1,14 @@
 import BLOG from '@/blog.config'
-import { Giscus } from '@giscus/react';
+import { Giscus } from '@giscus/react'
 
 /**
  * 評論插件
  * @returns {JSX.Element}
  * @constructor
  */
-const GiscusComponent = ({isDarkMode }) => {
-    const theme = isDarkMode ? 'dark': 'light'
-    return <Giscus
+const GiscusComponent = ({ isDarkMode }) => {
+    const theme = isDarkMode ? 'dark' : 'light'
+    return (<Giscus
         repo={BLOG.COMMENT_GISCUS_REPO}
         repoId={BLOG.COMMENT_GISCUS_REPO_ID}
         category={BLOG.COMMENT_GISCUS_CATEGORY}
@@ -21,7 +21,7 @@ const GiscusComponent = ({isDarkMode }) => {
         lang={BLOG.COMMENT_GISCUS_LANG}
         loading={BLOG.COMMENT_GISCUS_LOADING}
         crossorigin={BLOG.COMMENT_GISCUS_CROSSORIGIN}
-        />
+        />)
 }
 
 export default GiscusComponent
